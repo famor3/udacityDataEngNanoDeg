bff9a847b060710a630d00c8f99ff62903abfef6
Frank Amor <famor3@gmail.com>
2020-07-21 18:38:32 -0500

    Update sql_queries.py

--- Project1/sql_queries.py
+++ Project1/sql_queries.py
@@ -11,11 +11,11 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 songplay_table_create = ("""
                         CREATE TABLE IF NOT EXISTS songplays(
                         songplay_id SERIAL PRIMARY KEY,
-                        start_time timestamp,
-                        user_id int,
+                        start_time timestamp REFERENCES time(start_time),
+                        user_id int NOT NULL REFERENCES users(user_id),
                         level varchar,
-                        song_id varchar,
-                        artist_id varchar,
+                        song_id varchar REFERENCES songs(song_id),
+                        artist_id varchar REFERENCES artists(artist_id),
                         session_id int,
                         location varchar,
                         user_agent varchar)
@@ -23,26 +23,26 @@ songplay_table_create = ("""
 
 user_table_create = ("""
                         CREATE TABLE IF NOT EXISTS users(
-                        user_id int,
-                        first_name varchar,
-                        last_name varchar,
+                        user_id int PRIMARY KEY,
+                        first_name varchar NOT NULL,
+                        last_name varchar NOT NULL,
                         gender varchar,
                         level varchar)
                         """)
 
 song_table_create = ("""
                         CREATE TABLE IF NOT EXISTS songs(
-                        song_id varchar,
-                        title varchar,
-                        artist_id varchar,
+                        song_id varchar PRIMARY KEY,
+                        title varchar NOT NULL,
+                        artist_id varchar NOT NULL,
                         year int,
-                        duration float)
+                        duration float NOT NULL)
                         """)
 
 artist_table_create = ("""
                         CREATE TABLE IF NOT EXISTS artists(
                         artist_id varchar PRIMARY KEY,
-                        name varchar,
+                        name varchar NOT NULL,
                         location varchar,
                         latitude float,
                         longitude float)
@@ -50,7 +50,7 @@ artist_table_create = ("""
 
 time_table_create = ("""
                         CREATE TABLE IF NOT EXISTS time(
-                        start_time timestamp,
+                        start_time timestamp PRIMARY KEY,
                         hour int,
                         day int,
                         week int,
@@ -77,7 +77,8 @@ user_table_insert = ("""INSERT INTO users(
                         first_name, 
                         last_name, 
                         gender, 
-                        level) VALUES (%s, %s, %s, %s, %s);
+                        level) VALUES (%s, %s, %s, %s, %s)
+                        ON CONFLICT (user_id) DO UPDATE SET level = excluded.level;
                         """)
 
 song_table_insert = ("""INSERT INTO songs(
@@ -85,7 +86,8 @@ song_table_insert = ("""INSERT INTO songs(
                         title, 
                         artist_id, 
                         year, 
-                        duration) VALUES (%s, %s, %s, %s, %s);
+                        duration) VALUES (%s, %s, %s, %s, %s)
+                        ON CONFLICT (song_id) DO NOTHING;
                         """)
 
 artist_table_insert = ("""INSERT INTO artists(
@@ -104,7 +106,8 @@ time_table_insert = ("""INSERT INTO time(
                         week, 
                         month, 
                         year, 
-                        weekday) VALUES (%s, %s, %s, %s, %s, %s, %s);
+                        weekday) VALUES (%s, %s, %s, %s, %s, %s, %s)
+                        ON CONFLICT (start_time) DO NOTHING;
                         """)
 
 # FIND SONGS
@@ -118,5 +121,5 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
